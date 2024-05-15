6bfd4f7033b9d8982a1696c49ca3efd9bd710922
Baraka <raphaelbaraka424@gmail.com>
2024-05-15 09:47:10 +0300

    add beneficiaries

---
+++ src/components/AddBeneficiaries/AddBeneficiaries.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+function AddBeneficiaries() {
+  return (
+    <Box>
+        <Box>
+            
+        </Box>
+      
+    </Box>
+  )
+}
+
+export default AddBeneficiaries
