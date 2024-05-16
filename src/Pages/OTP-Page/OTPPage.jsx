00b2a885ddc03f531501cd1c890c823603b93552
lazo <lwachira742@gmail.com>
2024-05-16 17:01:51 +0300

    otp page

---
+++ src/Pages/OTP-Page/OTPPage.jsx
@@ -0,0 +1,42 @@
+import './OTPPage.css'
+const OTPPage = () => {
+  return (
+    <div className="login-page">
+        <div className="left-side">
+        <div className="left-side-content">
+          <h1>The Ultimate</h1>
+          <h1 className='mobile-banking'>Mobile banking</h1>
+          <h1>Experience.</h1>
+        </div>
+      </div>
+    < div className="right-side">
+    <div className="logo"></div>
+    <div className="otp">
+          OTP
+        </div>
+        <div className="sign-in-to-continue">
+          Sign in to continue. Your password is yours, do not share it with anyone.
+        </div>
+        <div className="container-2">
+          <div className="rectangle-6">
+          </div>
+          <div className="rectangle-7">
+          </div>
+          <div className="rectangle-81">
+          </div>
+          <div className="rectangle-9">
+          </div>
+          <div className="rectangle-10">
+          </div>
+          <div className="rectangle-11">
+          </div>
+        </div>
+    <button className="login-button">Login</button>
+      </div>
+      </div>   
+  )
+}
+
+export default OTPPage
+
+  
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardPage from '../Pages/DashboardPage';
-import LoginPage from "src/Pages/LoginPage";
-
+import LoginPage from "src/Pages/LoginPage"
+import OTPPage from "src/Pages/OTP-Page/OTPPage";
 function AppRoutes () {
     return (
         <Router>
             <Routes>
               <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/" element={<LoginPage />} />  
-  
+                <Route path="/" element={<LoginPage />} /> 
+                <Route path="/otp" element={<OTPPage />} /> 
             </Routes>
         </Router>
     )
