ca8eede47d351a4906e98307b8a7231f2a3dc33c
Baraka <raphaelbaraka424@gmail.com>
2024-05-16 14:24:34 +0300

    changes

--- src/components/AddBeneficiaries/AddBeneficiaries.jsx
+++ src/components/AddBeneficiaries/AddBeneficiaries.jsx
@@ -1,38 +1,143 @@
-import { Box, Typography, Button, TextField, MenuItem } from '@mui/material'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Box, Typography, Button, TextField, MenuItem } from '@mui/material';
+import arrow from "../../assets/arrow.svg"
+import { useState } from 'react';
 
 function AddBeneficiaries() {
+  const [selectedService, setSelectedService] = useState(null);
+
+  const handleServiceClick = (service) => {
+    setSelectedService(service);
+  };
+
   return (
-    <Box sx={{  backgroundColor: "whitesmoke", height: "380px", marginTop: "0px" }}>
-      <Box sx={{ display: "flex", gap: "400px" }}>
-        <Typography sx={{ color: "#01215B", fontWeight: "bold", fontFamily: "montserrat",marginLeft:"20px" }}>
+    <Box sx={{ backgroundColor: "whitesmoke", height: "380px", marginTop: "0px" }}>
+      <Box sx={{ display: "flex", gap: "450px" }}>
+        <Typography sx={{ color: "#01215B", fontWeight: "bold", fontFamily: "montserrat", marginLeft: "20px" }}>
           Add Beneficiary
         </Typography>
-        <Box sx={{ display: "flex" }}>
-          <Typography sx={{ color: "#A38526", fontWeight: "bold", fontFamily: "montserrat" }}>
+        <Box sx={{ display: "flex",gap:"4px" }}>
+          <Typography sx={{ color: "#A38526", fontWeight: "bold", fontFamily: "montserrat", fontSize: "15px" }}>
             MANAGE BENEFICIARIES
           </Typography>
-          <ArrowForwardIcon />
+          <Box >
+            <img src={arrow} />
+          </Box>
+          
         </Box>
       </Box>
-      <Box sx={{ backgroundColor: "white", marginLeft: "20px", marginRight: "20px", borderRadius: "6px", height: "320px" }}>
-        <Box sx={{ display: "flex", marginTop: "0px", paddingTop: "20px", marginLeft: "20px", gap: "10px" }}>
-          <Button sx={{ textTransform: 'none', backgroundColor: "#031F4F !important", color: "#FFFFFF", width: "120px", fontFamily: "montserrat" }}>
+      <Box sx={{ backgroundColor: "white", marginLeft: "20px", marginRight: "20px", borderRadius: "6px", height: "265px" }}>
+        <Box sx={{ display: "flex", marginTop: "0px", paddingTop: "10px", paddingRight: "10px", marginLeft: "20px", gap: "10px" }}>
+          <Button
+            onClick={() => handleServiceClick("Within Bank")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "Within Bank" ? "#031F4F" : "white",
+              color: selectedService === "Within Bank" ? "#FFFFFF" : "#031F4F",
+              width: "120px",
+              border: `2px solid ${selectedService === "Within Bank" ? "#031F4F" : "#01215B"}`,
+              fontFamily: "montserrat",
+              height: "30px",
+              textSize: "5px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "Within Bank" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             Within Bank
           </Button>
-          <Button sx={{ textTransform: 'none', color: "#A38526", border: '2px solid #01215B', fontFamily: "montserrat" }}>
+          <Button
+            onClick={() => handleServiceClick("Other Banks - RTGS")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "Other Banks - RTGS" ? "#031F4F" : "white",
+              color: selectedService === "Other Banks - RTGS" ? "#FFFFFF" : "#A38526",
+              border: `2px solid ${selectedService === "Other Banks - RTGS" ? "#031F4F" : "#01215B"}`,
+              fontFamily: "montserrat",
+              height: "30px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "Other Banks - RTGS" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             Other Banks - RTGS
           </Button>
-          <Button sx={{ textTransform: 'none', color: "#A38526", border: '2px solid #01215B', fontFamily: "montserrat" }}>
+          <Button
+            onClick={() => handleServiceClick("Other Banks - EFT")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "Other Banks - EFT" ? "#031F4F" : "white",
+              color: selectedService === "Other Banks - EFT" ? "#FFFFFF" : "#A38526",
+              border: `2px solid ${selectedService === "Other Banks - EFT" ? "#031F4F" : "#01215B"}`,
+              fontFamily: "montserrat",
+              height: "30px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "Other Banks - EFT" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             Other Banks - EFT
           </Button>
-          <Button sx={{ textTransform: 'none', color: "#A38526", border: '2px solid #01215B', width: "110px", fontFamily: "montserrat" }}>
+          <Button
+            onClick={() => handleServiceClick("International")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "International" ? "#031F4F" : "white",
+              color: selectedService === "International" ? "#FFFFFF" : "#A38526",
+              border: `2px solid ${selectedService === "International" ? "#031F4F" : "#01215B"}`,
+              width: "107px",
+              fontFamily: "montserrat",
+              height: "30px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "International" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             International
           </Button>
-          <Button sx={{ textTransform: 'none', color: "#A38526", border: '2px solid #01215B', width: "100px", fontFamily: "montserrat" }}>
+          <Button
+            onClick={() => handleServiceClick("Pesalink")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "Pesalink" ? "#031F4F" : "white",
+              color: selectedService === "Pesalink" ? "#FFFFFF" : "#A38526",
+              border: `2px solid ${selectedService === "Pesalink" ? "#031F4F" : "#01215B"}`,
+              width: "90px",
+              fontFamily: "montserrat",
+              height: "30px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "Pesalink" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             Pesalink
           </Button>
-          <Button sx={{ textTransform: 'none', color: "#A38526", border: '2px solid #01215B', width: "110px", fontFamily: "montserrat" }}>
+          <Button
+            onClick={() => handleServiceClick("mPesa")}
+            sx={{
+              textTransform: 'none',
+              backgroundColor: selectedService === "mPesa" ? "#031F4F" : "white",
+              color: selectedService === "mPesa" ? "#FFFFFF" : "#A38526",
+              border: `2px solid ${selectedService === "mPesa" ? "#031F4F" : "#01215B"}`,
+              width: "80px",
+              fontFamily: "montserrat",
+              height: "30px",
+              cursor: "pointer", // Adding cursor here
+              '&:hover': {
+                backgroundColor: selectedService === "mPesa" ? "#031F4F" : "white",
+                borderColor: "#01215B", // Change border color on hover
+              },
+            }}
+          >
             mPesa
           </Button>
         </Box>
@@ -40,7 +145,7 @@ function AddBeneficiaries() {
           Beneficiary Bank Details
         </Typography>
 
-        <Box sx={{ display: "flex", gap: "60px", marginLeft: "20px", marginTop: "10px" }}>
+        <Box sx={{ display: "flex", gap: "60px", marginLeft: "20px", marginTop: "5px" }}>
           <TextField sx={{ width: "350px", backgroundColor: "whitesmoke", '& input': { height: '14px' }, marginTop: '0px' }}
             label="Account Number"
           />
@@ -52,17 +157,21 @@ function AddBeneficiaries() {
           >
           </TextField>
         </Box>
-        <br />
-        <Box sx={{ display: "flex", gap: "60px", marginLeft: "20px", marginTop: "0px", '& input': { height: '14px' } }}>
-
+      
+        <Box sx={{ display: "flex", gap: "60px", marginLeft: "20px", marginTop: "13px", '& .MuiSelect-selectMenu': { paddingTop: '4px', paddingBottom: '4px' } }}>
           <TextField
             select
             label="Select Currency"
             sx={{
               width: "350px",
               backgroundColor: "whitesmoke",
-              '& .MuiSelect-select': {
-                height: '4px' // Adjust the height as needed
+              '& .MuiSelect-selectMenu': {
+                paddingTop: '4px',
+                paddingBottom: '4px',
+                maxHeight: '100px',
+              },
+              '& .MuiInputBase-input': {
+                height: '4px'
               }
             }}
           >
@@ -71,12 +180,18 @@ function AddBeneficiaries() {
           </TextField>
 
           <TextField
-            sx={{ width: "350px", backgroundColor: "whitesmoke", height: "46px", '& input': { height: '14px' }, marginTop: '0px' }}
+            sx={{
+              width: "350px",
+              backgroundColor: "whitesmoke",
+              height: "46px",
+              '& input': { height: '14px' },
+              marginTop: '0px'
+            }}
             label="Re-Enter Account Number"
           />
-
         </Box>
-        <Button sx={{ textTransform: 'none', backgroundColor: "#031F4F !important", color: "#FFFFFF", width: "760px", marginLeft: "20px", marginTop: "20px" }} >
+
+        <Button sx={{ textTransform: 'none', backgroundColor: "#031F4F !important", color: "#FFFFFF", width: "760px", marginLeft: "20px", marginTop: "10px",fontFamily: "montserrat" }} >
           ADD BENEFICIARY +
         </Button>
       </Box>
@@ -85,4 +200,4 @@ function AddBeneficiaries() {
   )
 }
 
-export default AddBeneficiaries
+export default AddBeneficiaries;
