7f1748c64cdd528d024bb26e829cfd623a9e04bb
lazo <lwachira742@gmail.com>
2024-05-16 17:10:07 +0300

    otp page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardPage from '../Pages/DashboardPage';
-import LoginPage from "src/Pages/LoginPage"
+import LoginPage from "src/Pages/LoginPage";
 import OTPPage from "src/Pages/OTP-Page/OTPPage";
 function AppRoutes () {
     return (
