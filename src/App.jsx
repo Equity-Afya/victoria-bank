5e9b770276cc008ffe167870ab18d451edeac4ee
enthusiastic-ivic <victorakelo95@gmail.com>
2024-05-16 09:48:24 +0300

    MyAccount component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header/Header";
+import MyAccount from "./components/MyAccount/MyAccount";
 
 function App() {
-
-  
+  return (
+    <div>
+      <Header></Header>
+      <MyAccount></MyAccount>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ATM-card/ATMcard.jsx
@@ -0,0 +1,27 @@
+import "./ATMcard.css";
+
+const ATMcard = () => {
+  return (
+    <div className="atm-card">
+      <div className="container-9">
+        <div className="container-36">
+          <img className="group-4" src="./src/assets/vectors/logo.svg" />
+          <div className="vector">
+            <img className="vector-50" src="./src/assets/vectors/chip.svg" />
+          </div>
+        </div>
+        <div className="container-54">
+          <div className="container-26">
+            <div className="eliud-njogu">Eliud Njogu</div>
+            <span className="container-12">0010 0048 3290</span>
+          </div>
+          <div className="rectangle-11">
+            <div className="usd">USD</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ATMcard;
