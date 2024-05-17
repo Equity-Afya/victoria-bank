c6d1199f28b7f683f7d71303b46ecd816505abe9
Mbarak-jr <mohammedmbarak77@gmail.com>
2024-05-17 09:12:39 +0300

    updated manage beneficiaries page

--- src/Pages/ManageBeneficiariesPage.jsx
+++ src/Pages/ManageBeneficiariesPage.jsx
@@ -1,21 +1,31 @@
 
 import Sidebar from '../components/Sidebar/Sidebar'
 import ManageBeneficiaries from '../components/ManageBeneficiaries/ManageBeneficiaries'
-import Header from '../components/Header/Header'
+import MyAccount from 'src/components/MyAccount/MyAccount'
+import { Box } from '@mui/material'
+import Footer from 'src/components/Footer/Footer'
+import BottomCard from 'src/components/BottomCard/BottomCard'
 
 const ManageBeneficiariesPage = () => {
   return (
-    <>
-    <div>
+  
+    <Box  sx={{display: 'flex', marginLeft: '50px'}}>
+      <Box>
       <Sidebar/>
-    </div>
-    <div>
-      <ManageBeneficiaries/>
-    </div>
-    <div>
-      <Header/>
-    </div>
-    </>
+      </Box>
+      <Box>
+        <Box sx={{display: 'flex'}}>
+        <MyAccount/>
+        <Box sx={{marginLeft: '20px', marginTop: '50px'}}>
+        <ManageBeneficiaries/>
+        </Box>
+        </Box>
+      <BottomCard/>
+       <Footer/>
+      </Box>
+      
+    </Box>
+  
   )
 }
 
